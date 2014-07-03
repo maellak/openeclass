52fb68fe42b3b851d6ea19ce63b03ae6b8edf2a7
Dimosthenis Nikoudis <dnna@dnna.gr>
2014-07-03 17:40:15 +0300

    Added login mechanism and GetCourses REST API function

--- modules/rest/index.php
+++ modules/rest/index.php
@@ -1,4 +1,23 @@
 <?php
-require __DIR__.'/../../include/Slim/Slim.php';
+// Initialize openClass libraries
+define('RESPONSE_FAILED', json_encode(array('status' => 'FAILED')));
+
+require __DIR__ . '/../../include/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+$app->config('debug', true);
+error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE ^ E_WARNING);
+
+// Setup the REST routes
+require_once (__DIR__.'/auth.php');
+$app->map('/login', RequestAccessToken)->via('POST');
+require_once (__DIR__.'/courses.php');
+$app->map('/courses', CheckAuth, GetCourses)->via('GET');
+$app->map('/courses', CheckAuth, PostCourses)->via('POST');
+$app->map('/courses', CheckAuth, DeleteCourses)->via('DELETE');
+// 404 not found
+$app->notFound(function () { echo json_encode(array('status' => 'NOT_FOUND')); });
+
+$app->run();
 ?>
