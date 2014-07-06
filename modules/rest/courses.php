307c1600721783911fb157371fe91cf8005b59e1
Dimosthenis Nikoudis <dnna@dnna.gr>
2014-07-06 18:58:01 +0300

    Remove GetCourses content to make it part of exercise

--- modules/rest/courses.php
+++ modules/rest/courses.php
@@ -1,25 +1,6 @@
 <?php
 function GetCourses() {
-    $courses = array();
-
-    $sql = "SELECT course.code,
-                   course.lang,
-                   course.title,
-                   course.keywords,
-                   course.visible,
-                   course.prof_names,
-                   course.public_code,
-                   course_user.status as status
-             FROM course JOIN course_user ON course.id = course_user.course_id
-             WHERE course_user.user_id = ?d
-             ORDER BY status, course.title, course.prof_names";
-
-    $callback = function($course) use (&$courses) {
-        $courses[] = $course;
-    };
-
-    Database::get()->queryFunc($sql, $callback, intval($_SESSION['uid']));
-    echo json_encode($courses);
+    echo 'GET';
 }
 function PostCourses() {
     echo 'POST';
