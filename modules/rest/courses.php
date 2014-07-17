c3c31e65fbeac8e94f7e8d8d7fcee8280de17035
papadakisemmanouel <papadakisemmanouel@gmail.com>
2014-07-17 15:23:44 +0300

    dior8wsh

--- modules/rest/courses.php
+++ modules/rest/courses.php
@@ -1,37 +1,8 @@
 <?php
 function GetCourses() {
-   
-//$movies = array("Dramas"=>5,"Comedies"=>6);
-
-//echo json_encode($movies);
-
-//pairnoume ti vasi mas, vazoume ti vasi diladi se mia metavliti
-
-$database = Database::get();
-$courses = array();
-
-$database->queryFunc
-(
-
-'SELECT title FROM course', function($row) use (&$courses) 
-{
-
-$courses[] = $row;
-}
-
-);
-
-//var_dump($row);
-
-echo json_encode($courses);
-
-
-// echo 'GET';
-}
-function PostCourses() {
-    echo 'POST';
-}
-function DeleteCourses() {
-    echo 'DELETE';
+	$course=array();
+	$database = Database::get();
+	$database->queryFunc('SELECT code,lang,title,keywords,visible,prof_names,public_code FROM course', function($row)use (&$course) {$course[] = $row;} );
+	echo json_encode($course);
 }
 ?>
