eaa91e9571cb0464a9c33eeb23f3cc1c174e8d6c
papadakisemmanouel <papadakisemmanouel@gmail.com>
2014-07-18 13:30:53 +0300

    added another error option

--- modules/rest/enrolled_courses.php
+++ modules/rest/enrolled_courses.php
@@ -45,29 +45,34 @@ function PostEnrollCourse() {
 	$PASS_PROTECTED = 2;
 	$COURSE_NOT_EXISTS = 3;
 	$ALREADY_ENROLLED = 4;
-	$uid = $_SESSION['uid'];
-	$cid = $_POST['cid'];
-	$course_info = Database::get()->querySingle("SELECT public_code, password, visible FROM course WHERE id = ?d", $cid);
-	if ($course_info) {
-		$is_enrolled = Database::get()->querySingle("SELECT * FROM `course_user` WHERE `course_id` = ?d AND `user_id` = ?d", $cid, intval($uid));
-		if (!$is_enrolled) {
-			if (($course_info->visible == COURSE_REGISTRATION or
-					$course_info->visible == COURSE_OPEN) and !empty($course_info->password) and
-					$course_info->password !== autounquote($_POST['pass' . $cid])) {
-				echo $PASS_PROTECTED;
-			}
-			if (is_restricted($cid) and !in_array($cid, $selectCourse)) { // do not allow registration to restricted course
-				echo $RESTRICTED;
+	$NOT_LOGGED_IN = 5;
+	if (!isset($_SESSION)) {
+		$uid = $_SESSION['uid'];
+		$cid = $_POST['cid'];
+		$course_info = Database::get()->querySingle("SELECT public_code, password, visible FROM course WHERE id = ?d", $cid);
+		if ($course_info) {
+			$is_enrolled = Database::get()->querySingle("SELECT * FROM `course_user` WHERE `course_id` = ?d AND `user_id` = ?d", $cid, intval($uid));
+			if (!$is_enrolled) {
+				if (($course_info->visible == COURSE_REGISTRATION or
+						$course_info->visible == COURSE_OPEN) and !empty($course_info->password) and
+						$course_info->password !== autounquote($_POST['pass' . $cid])) {
+					echo $PASS_PROTECTED;
+				}
+				if (is_restricted($cid) and !in_array($cid, $selectCourse)) { // do not allow registration to restricted course
+					echo $RESTRICTED;
+				} else {
+					Database::get()->query("INSERT IGNORE INTO `course_user` (`course_id`, `user_id`, `status`, `reg_date`)
+		                                        VALUES (?d, ?d, ?d, CURDATE())", $cid, intval($uid), USER_STUDENT);
+					echo $SUCCESS;
+				}
 			} else {
-				Database::get()->query("INSERT IGNORE INTO `course_user` (`course_id`, `user_id`, `status`, `reg_date`)
-	                                        VALUES (?d, ?d, ?d, CURDATE())", $cid, intval($uid), USER_STUDENT);
-				echo $SUCCESS;
+				echo $ALREADY_ENROLLED;
 			}
 		} else {
-			echo $ALREADY_ENROLLED;
+			echo $COURSE_NOT_EXISTS;
 		}
 	} else {
-		echo $COURSE_NOT_EXISTS;
+		echo $NOT_LOGGED_IN;
 	}
 }
 ?>
