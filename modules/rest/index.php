7bf46fe763a95ce9f72c608c133163dc6e1688fc
Ioannis Zikai <cs111064@teiath.gr>
2014-07-18 14:14:51 +0300

    Task 1: unroll.

--- modules/rest/index.php
+++ modules/rest/index.php
@@ -1,6 +1,22 @@
 <?php
-// Initialize openClass libraries
 define('RESPONSE_FAILED', json_encode(array('status' => 'FAILED')));
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+    if (isset($_SERVER['HTTP_ORIGIN'])) {
+        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+        header('Access-Control-Allow-Credentials: true');
+        header('Access-Control-Max-Age: 86400');    // cache for 1 day
+    }
+    // Access-Control headers are received during OPTIONS requests
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+            header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+
+        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    }
 
 require __DIR__ . '/../../include/Slim/Slim.php';
 \Slim\Slim::registerAutoloader();
@@ -10,12 +26,20 @@ $app->config('debug', true);
 error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE ^ E_WARNING);
 
 // Setup the REST routes
+require_once (__DIR__.'/../../include/init.php');
 require_once (__DIR__.'/auth.php');
-$app->map('/login', RequestAccessToken)->via('POST');
+$app->map('/login', RequestAccessToken)->via('POST', 'OPTIONS');
 require_once (__DIR__.'/courses.php');
-$app->map('/courses', GetCourses)->via('GET');
-$app->map('/courses', PostCourses)->via('POST');
-$app->map('/courses', DeleteCourses)->via('DELETE');
+$app->map('/courses', GetCourses)->via('GET', 'OPTIONS');
+$app->map('/courses/announcements/:aid/read', CheckAuth, function() use($app) {
+	$ann_id = $_POST['aid'];
+	postReadCourses($ann_id);
+  })->via('POST', 'OPTIONS');
+$app->map('/courses/:cid', CheckAuth, function() use($app) {
+	//Getting the parameter which is suppossed to be delivered via the Delete/Unenroll button
+	$course_id = $_POST['cid'];
+	DeleteCourses($course_id);
+  })->via('DELETE', 'OPTIONS');
 // 404 not found
 $app->notFound(function () { echo json_encode(array('status' => 'NOT_FOUND')); });
 
