ff18b8f13c4f622a7734e751c4baf681ed465a96
ckaltsidis <ckaltsidis@gmail.com>
2014-07-18 15:22:05 +0300

    Add posts functionality
    Issue #31, #32

--- modules/rest/courses.php
+++ modules/rest/courses.php
@@ -25,16 +25,21 @@ function GetTopics($cid, $fid) {
 }
 
 function PostTopic($cid, $fid) {
+		
 	define('RESPONSE_FAILED', json_encode(array('status' => 'FAILED')));
 	$_POST = json_decode(file_get_contents('php://input'), true);
 	
 	// Ανάκτηση του τίτλου του topic
 	$topic = $_POST['title'];
+	// Ανακτηση του poster_id
+	$poster_id = $_SESSION['uid'];
 	
 	// Αποθήκευση του topic
-	Database::get()->query("INSERT INTO forum_topic (forum_topic.title, forum_topic.topic_time, forum_topic.forum_id)
-                                              VALUES (?s, NOW(), $fid)", $topic);
-	session_regenerate_id();
+	Database::get()->query("INSERT INTO forum_topic (forum_topic.title, 
+													forum_topic.poster_id,
+													forum_topic.topic_time, 
+													forum_topic.forum_id)
+                                              VALUES (?s, ?d, NOW(), ?d)", $topic, intval($poster_id), intval($fid));
 	
 	// Ανακτούμε τον αριθμό των topics που περιέχει το συγκεκριμένο forum για να τον ενημερώσουμε
 	$sqlNumTopics = 'SELECT num_topics FROM forum WHERE forum.id = '.$fid;
@@ -45,9 +50,44 @@ function PostTopic($cid, $fid) {
 	
 	// Ενημέρωση του αριθμού των topics στον πίνακα forum
 	Database::get()->query("UPDATE forum SET num_topics = $num_topics WHERE forum.id = $fid");
-	session_regenerate_id();
 	
+	echo json_encode(array('success' => 'True'));	
+}
+
+function GetPosts($cid, $fid, $tid) {
+	$posts = array();
+	$database = Database::get();
+	$sql = 'SELECT * FROM forum_post WHERE topic_id = '.$tid;
+	$database->queryFunc($sql, function($row)use (&$posts) {$posts[] = $row;});
+	echo json_encode($posts);
+	
+}
+
+function PostPosts($cid, $fid, $tid) {
+	define('RESPONSE_FAILED', json_encode(array('status' => 'FAILED')));
+	$_POST = json_decode(file_get_contents('php://input'), true);
+	
+	// Ανάκτηση των στοιχείων του post
+	$post_text = $_POST['post_text'];
+	$poster_id = $_SESSION['uid'];
+	$parent_post_id = 0; // Εδώ τι βάζουμε άραγε;;;
+		
+	// Αποθήκευση του post
+	$post = Database::get()->query("INSERT INTO forum_post (forum_post.topic_id, 
+													forum_post.post_text,
+													forum_post.poster_id,
+													forum_post.post_time,
+													forum_post.poster_ip,
+													forum_post.parent_post_id)
+							VALUES (?d, ?s, ?d, NOW(), ?s, ?d)", 
+                            	intval($tid), $post_text, intval($poster_id), $_SERVER['REMOTE_ADDR'], intval($parent_post_id));
+
+	// Ενημέρωση του last_post_id στον πίνακα forum_topic
+	$last_post_id = $post->lastInsertID;
+	Database::get()->query("UPDATE forum_topic SET last_post_id = $last_post_id WHERE forum_topic.id = $tid");
 	echo json_encode(array('success' => 'True'));
 	
 }
+
+
 ?>
