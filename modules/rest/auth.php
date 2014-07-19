0dabfc5c75fd8178583afa998103a72a3b72b843
root <root@snf-538265.vm.okeanos.grnet.gr>
2014-07-19 11:36:35 +0300

    fix path issue in auth.php

--- modules/rest/auth.php
+++ modules/rest/auth.php
@@ -17,7 +17,7 @@ function CheckAuth() {
     }
 
     global $mysqlServer, $mysqlUser, $mysqlPassword, $mysqlMainDb;
-    require_once ('../../include/init.php');
+    require_once (_DIR__ . '/../../include/init.php');
 }
 
 function RequestAccessToken() {
