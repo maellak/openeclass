3cf03014d227828ffb10f2f63f4758f0d72685c9
Ioannis Zikai <cs111064@teiath.gr>
2014-07-20 13:51:21 +0300

    Update read posts

--- modules/rest/index.php
+++ modules/rest/index.php
@@ -37,6 +37,7 @@ error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE ^ E_WARNING);
 // Setup the REST routes
 require_once (__DIR__.'/../../include/init.php');
 require_once (__DIR__.'/auth.php');
+require_once (__DIR__.'/nots.php');
 $app->map('/login', RequestAccessToken)->via('POST', 'OPTIONS');
 require_once (__DIR__.'/courses.php');
 
