1c7a726eb8031692fbc4150828aa225179ae4385
Leuteris <dennisritchiec@gmail.com>
2014-10-20 23:34:15 +0300

    commit
    commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-04 22:48
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '';
+$mysqlMainDb = 'eclass';
---
+++ courses/TMA100/index.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+$_SESSION['dbname']='TMA100';
+include '../../modules/course_home/course_home.php';
