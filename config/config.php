cf9ae8db31e7994c73373d90ed95432487dce5a8
Leuteris <dennisritchiec@gmail.com>
2014-10-21 09:52:58 +0300

    install commit
    install commit

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-21 09:46
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = '';
+$mysqlMainDb = 'eclass';
