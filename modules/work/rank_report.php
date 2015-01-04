bf50c7103ffd2c2165edd1e752f2683dfaf3ee3e
giannisdag <giannisdag@yahoo.gr>
2015-01-04 23:48:06 +0200

    fix metal images bug

--- modules/work/rank_report.php
+++ modules/work/rank_report.php
@@ -119,9 +119,9 @@ function get_table_content($assign,$submissions) {
        
        // Condition about rank position and color of medal
 
-if ($i==1 or $i == 2) {$i.=" <img src=\"http://".$_SERVER['HTTP_HOST'].$themeimg."/work_medals/Gold_medal_with_cup.svg\" width='30px' height='30px'>";}                                        
-if ($i==3 or $i == 4) {$i.=" <img src=\"http://".$_SERVER['HTTP_HOST'].$themeimg."/work_medals/Silver_medal_with_cup.svg\"  width='30px' height='30px'>";}     
-if ($i==5 or $i == 6) {$i.=" <img src=\"http://".$_SERVER['HTTP_HOST'].$themeimg."/work_medals/Bronze_medal_with_cup.svg\" width='30px' height='30px'>";} 
+if ($i==1 or $i == 2) {$i.=" <img src=\"http://".$_SERVER['HTTP_HOST'].$themeimg."/work_medals/Gold_medal_with_cup.png\" width='30px' height='30px'>";}                                        
+if ($i==3 or $i == 4) {$i.=" <img src=\"http://".$_SERVER['HTTP_HOST'].$themeimg."/work_medals/Silver_medal_with_cup.png\"  width='30px' height='30px'>";}     
+if ($i==5 or $i == 6) {$i.=" <img src=\"http://".$_SERVER['HTTP_HOST'].$themeimg."/work_medals/Bronze_medal_with_cup.png\" width='30px' height='30px'>";} 
 
  // End of Condition about rank position and color of medal    
     
