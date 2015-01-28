d5a2ac6167a40efaf35a45c8d319ab600e4e6fbd
Achilleas Pipinellis <axilleas@axilleas.me>
2015-01-28 18:21:18 +0200

    Remove unused files.

--- modules/announcements/read.php
+++
@@ -1,9 +0,0 @@
-<?php
-PostReadCourses($ann_id){
-    $uid = $_SESSION['user_id'];
-    $query = "INSERT INTO announcement_user (user_id, ann_id) VALUES ($uid, $ann_id)";
-    Database::get() -> querySingle($query);
-    alert("Announcment marked as read");
-}
-
-?>
